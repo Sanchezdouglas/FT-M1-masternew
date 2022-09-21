f80c8941d87df5dc6123b04072081b18c8218227
sancehzdouglas <97553140+Sanchezdouglas@users.noreply.github.com>
2022-09-21 13:43:41 +0200

    Act 2

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -15,10 +15,16 @@ function BinarioADecimal(num) {
 
 function DecimalABinario(num) {
   // tu codigo aca
-  
-
+  //Dividir num /2 hasta llegar a 0
+  let result = []; //[1,1,1]
+  // do -while == ejecuto => reviso y cont?
+  // while == reviso sin cont? => ejecuto
+  while (num > 0) { //15
+    result.push(num%2); //1/2 0 
+    num = Math.floor(num/2); //1
+  }
+  return result.reverse().join(""); //¨[1,1,1] => join() = "111";
 }
-
 
 module.exports = {
   BinarioADecimal,
